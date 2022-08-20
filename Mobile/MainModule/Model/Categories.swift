c58a0d0a2a59546a79cf558d7327beea84a980da
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-08-20 20:42:29 +0300

    Add Categories model

---
+++ Mobile/MainModule/Model/Categories.swift
@@ -0,0 +1,16 @@
+//
+//  Categories.swift
+//  Mobile
+//
+//  Created by tambanco 🥳 on 20.08.2022.
+//
+
+import Foundation
+
+struct Categories {
+    let phones: String
+    let computer: String
+    let health: String
+    let book: String
+    let other: String
+}
