569da712333396f735a52a8e995aff316afef9cc
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-08-20 18:20:38 +0300

    Update splash

--- Mobile/MainModule/View/CategoriesView.swift
+++ Mobile/MainModule/View/CategoriesView.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 class CategoriesView: UIView {
-
+    lazy var fiatCardView: UIView! = {
+        let view = UIView()
+        view.layer.cornerRadius = 15
+        return view
+    }()
+    
+    lazy var fiatHeader: UILabel! = {
+        let label = UILabel()
+        label.font = UIFont(name: "MarkPro", size: 17)
+        label.text = "Select Category"
+        return label
+    }()
+    
+    lazy var cancelButton: UIButton! = {
+        var config = UIButton.Configuration.borderless()
+        let button = UIButton(configuration: config)
+        button.setTitle("view all", for: .normal)
+        return button
+    }()
+    
+    
 
 }
