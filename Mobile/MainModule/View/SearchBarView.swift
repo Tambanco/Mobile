0fe5ada8b6a6b3a8376efa50b08d2077a050611e
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-08-21 13:33:58 +0300

    Add SearchBarView

---
+++ Mobile/MainModule/View/SearchBarView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBarView.swift
+//  Mobile
+//
+//  Created by tambanco 🥳 on 21.08.2022.
+//
+
+import UIKit
+
+class SearchBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
