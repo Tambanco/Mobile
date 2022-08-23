117355554bb64bb114c3a5f432382a2ee0e7a525
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-08-23 14:44:04 +0300

    Update SectionHeader

--- Mobile/MainModule/View/MainViewController.swift
+++ Mobile/MainModule/View/MainViewController.swift
@@ -15,7 +15,7 @@ class MainViewController: UIViewController {
     private var categoriesView: CategoriesView!
     //    private lazy var categoriesCollectionView = createCollectionView()
     private var searchBarView: SearchBarView!
-    private lazy var mainCollectionView = createCollectionView()
+    private lazy var mainCollectionReusableView = createCollectionView()
     //    private var hotSalesView: HotSalesView!
     //    private var hotSalesCollectionView: UICollectionView!
     private var categories: [String] = ["Phones", "Computer", "Health", "Books", "Other"]
@@ -27,28 +27,36 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMainView()
-        setupCategoriesView()
-        setupMainCollectionView()
-        setupSearchBarView()
+        setupMainCollectionReusableView()
+        //        setupCategoriesView()
+        //        setupSearchBarView()
         //        setupHotSalesView()
         //        setupHotSalesCollectionView()
     }
     
-    private func setupMainCollectionView() {
+    private func setupMainCollectionReusableView() {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
         layout.itemSize = CGSize(width: 90, height: 90)
         layout.scrollDirection = .horizontal
-        mainCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
-        mainCollectionView.backgroundColor = .green
+        mainCollectionReusableView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
         
-        view.addSubview(mainCollectionView)
+        mainCollectionReusableView.backgroundColor = .green
         
-        mainCollectionView.snp.makeConstraints { make in
+        mainCollectionReusableView.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.reuseId)
+        mainCollectionReusableView.register(SectionHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
+        
+        mainCollectionReusableView.dataSource = self
+        mainCollectionReusableView.delegate = self
+        
+        view.addSubview(mainCollectionReusableView)
+        
+        mainCollectionReusableView.snp.makeConstraints { make in
             make.leading.equalToSuperview()
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.trailing.equalToSuperview()
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+//            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+            make.height.equalTo(100)
         }
     }
     
@@ -90,36 +98,36 @@ class MainViewController: UIViewController {
     //        }
     //    }
     
-    private func setupSearchBarView() {
-        searchBarView = SearchBarView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        view.addSubview(searchBarView)
-        
-        searchBarView.searchBar.searchTextField.clipsToBounds = true
-        searchBarView.searchBar.searchTextField.layer.cornerRadius = 17
-        
-        searchBarView.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.top.equalTo(categoriesView.snp.bottom)
-            make.trailing.equalToSuperview()
-            make.height.equalTo(60)
-        }
-    }
+    //    private func setupSearchBarView() {
+    //        searchBarView = SearchBarView(frame: CGRect.zero)
+    //        view.addSubview(searchBarView)
+    //
+    //        searchBarView.searchBar.searchTextField.clipsToBounds = true
+    //        searchBarView.searchBar.searchTextField.layer.cornerRadius = 17
+    //
+    //        searchBarView.snp.makeConstraints { make in
+    //            make.leading.equalToSuperview()
+    //            make.top.equalTo(categoriesView.snp.bottom)
+    //            make.trailing.equalToSuperview()
+    //            make.height.equalTo(60)
+    //        }
+    //    }
     
     private func setupMainView() {
         self.view.backgroundColor = UIColor(hexString: "#E5E5E5")
     }
     
-    private func setupCategoriesView() {
-        categoriesView = CategoriesView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        view.addSubview(categoriesView)
-        
-        categoriesView.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.top.equalTo(self.view.safeAreaLayoutGuide)
-            make.trailing.equalToSuperview()
-            make.height.equalTo(200)
-        }
-    }
+    //    private func setupCategoriesView() {
+    //        categoriesView = CategoriesView(frame: CGRect.zero)
+    //        view.addSubview(categoriesView)
+    //
+    //        categoriesView.snp.makeConstraints { make in
+    //            make.leading.equalToSuperview()
+    //            make.top.equalTo(self.view.safeAreaLayoutGuide)
+    //            make.trailing.equalToSuperview()
+    //            make.height.equalTo(200)
+    //        }
+    //    }
     
     //    private func setupCollectionView() {
     //        let layout = UICollectionViewFlowLayout()
@@ -147,41 +155,77 @@ class MainViewController: UIViewController {
     //    }
 }
 
-// MARK: - CollectionView methods
-extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 3
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return categories.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.reuseId, for: indexPath) as! CategoryCell
         cell.categoryLabel.text = categories[indexPath.row]
         cell.categoryImageView.image = UIImage(named: "\(categories[indexPath.row])")
-
+        cell.backgroundColor = .systemBlue
+        
         return cell
     }
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell else {
-            return
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as! SectionHeader
+            sectionHeader.label.text = "TRENDING"
+            sectionHeader.backgroundColor = .systemOrange
+            return sectionHeader
+        } else { //No footer in this case but can add option for that
+            return UICollectionReusableView()
         }
-        cell.ellipseView.backgroundColor = UIColor(hexString: "FF6E4E")
-        cell.categoryLabel.textColor = UIColor(hexString: "FF6E4E")
-
     }
-
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell else {
-            return
-        }
-        cell.ellipseView.backgroundColor = UIColor(hexString: "FFFFFF")
-        cell.categoryLabel.textColor = UIColor(hexString: "010035")
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 40)
     }
+    
 }
+
+//// MARK: - CollectionView methods
+//extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+//
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 3
+//    }
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return categories.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.reuseId, for: indexPath) as! CategoryCell
+//        cell.categoryLabel.text = categories[indexPath.row]
+//        cell.categoryImageView.image = UIImage(named: "\(categories[indexPath.row])")
+//
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell else {
+//            return
+//        }
+//        cell.ellipseView.backgroundColor = UIColor(hexString: "FF6E4E")
+//        cell.categoryLabel.textColor = UIColor(hexString: "FF6E4E")
+//
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+//        guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell else {
+//            return
+//        }
+//        cell.ellipseView.backgroundColor = UIColor(hexString: "FFFFFF")
+//        cell.categoryLabel.textColor = UIColor(hexString: "010035")
+//    }
+//}
 
 // MARK: - Binding
 extension MainViewController: MainViewProtocol {
---
+++ Mobile/MainModule/View/SectionHeader.swift
@@ -0,0 +1,42 @@
+//
+//  CollectionReusableView.swift
+//  Mobile
+//
+//  Created by tambanco 🥳 on 23.08.2022.
+//
+
+import UIKit
+
+class SectionHeader: UICollectionReusableView {
+    
+    lazy var label = createLabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(label)
+        
+        label.backgroundColor = .blue
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+                 label.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+                 label.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20).isActive = true
+                 label.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+fileprivate extension SectionHeader {
+    private func createLabel() -> UILabel {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        label.sizeToFit()
+        return label
+    }
+}
