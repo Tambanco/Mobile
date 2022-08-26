690e3709f7bec73e9f651f3c1f66efd5a01d642f
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-08-27 00:29:08 +0300

    Add Router

---
+++ Mobile/ServiceLayer/Router.swift
@@ -0,0 +1,50 @@
+//
+//  Router.swift
+//  Mobile
+//
+//  Created by tambanco 🥳 on 27.08.2022.
+//
+
+import Foundation
+import UIKit
+
+protocol RouterMain {
+    var navigationController: UINavigationController? { get set }
+    var assemblyBuilder: AssemblyBuilderProtocol? { get set }
+}
+
+protocol RouterProtocol: RouterMain {
+    func initialViewController()
+    func showCurrencyView()
+    func popToRoot()
+}
+
+class Router: RouterProtocol {
+    var navigationController: UINavigationController?
+    var assemblyBuilder: AssemblyBuilderProtocol?
+    
+    init(navigationController: UINavigationController, assemblyBuilder: AssemblyBuilderProtocol) {
+        self.navigationController = navigationController
+        self.assemblyBuilder = assemblyBuilder
+    }
+    
+    func initialViewController() {
+        if let navigationController = navigationController {
+            guard let fiatViewController = assemblyBuilder?.createFiatModule(router: self) else { return }
+            navigationController.pushViewController(fiatViewController, animated: true)
+        }
+    }
+    
+    func showCurrencyView() {
+        if let navigationController = navigationController {
+            guard let currencyViewController = assemblyBuilder?.createCurrencyModule(router: self) else { return }
+            navigationController.pushViewController(currencyViewController, animated: true)
+        }
+    }
+    
+    func popToRoot() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
+}
