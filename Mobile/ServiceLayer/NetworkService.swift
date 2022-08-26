791433566b0ca69dcdb03da0f92e0cb71aec85e9
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-08-26 23:54:34 +0300

    Add NetworkLayer

---
+++ Mobile/ServiceLayer/NetworkService.swift
@@ -0,0 +1,61 @@
+//
+//  NetworkService.swift
+//  Mobile
+//
+//  Created by tambanco 🥳 on 26.08.2022.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
+
+protocol NetworkServiceProtocol: Codable {
+    func getCurrencyList(completion: @escaping (Result<[String]?, Error>) -> Void)
+    func convertTwoCurrensies(from:String, to: String, amount: String, completion: @escaping (Result<String?, Error>) -> Void)
+}
+
+final class NewtworkService: NetworkServiceProtocol {
+    func getCurrencyList(completion: @escaping (Result<[String]?, Error>) -> Void) {
+        let urlString = "https://api.exchangerate.host/symbols"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data else {
+                completion(.failure(error!))
+                return
+            }
+            
+            let json = String(data: data, encoding: .utf8)!.data(using: .utf8)!
+            var fiatList: [String] = []
+            fiatList = JSONParser.parseCurrencyList(json: json)
+            completion(.success(fiatList))
+        }
+        task.resume()
+    }
+    
+    func convertTwoCurrensies(from: String, to: String, amount: String, completion: @escaping (Result<String?, Error>) -> Void) {
+        let urlString = "https://api.exchangerate.host/convert?from=\(from)&to=\(to)&amount=\(amount)"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data else {
+                completion(.failure(error!))
+                return
+            }
+            
+            let json = String(data: data, encoding: .utf8)!.data(using: .utf8)!
+            var convertedResult: String?
+            convertedResult = JSONParser.parseConvertedResult(json: json)
+            completion(.success(convertedResult))
+        }
+        task.resume()
+    }
+}
